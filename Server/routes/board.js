5021ceb60ba5b915cf5e617102e958d2b49d77b2
ParkHyunSoo <parkhyuns00@naver.com>
2023-08-22 10:55:19 +0900

    TASK #16 : 등록된 모든 게시물 리스트 반환 및 특정 사용자가 등록한 모든 게시물 리스트 반환 요청 받는 라우터

--- Server/routes/board.js
+++ Server/routes/board.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 const boardSystem = require('../services/boardSystem.js');
+const likeSystem = require('../services/likeSystem.js');
+const bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
+
+// 등록된 게시물 리스트 가져오기 요청
+router.post('/boardlist', (req, res) => {
+    const postData = req.body;
+
+    console.log(postData);
+
+    // 사용자의 ID 값이 전달 됐는지 판단 -> 전달 안됐다면 전체 게시물 리스트, 전달 됐다면 사용자가 등록한 게시물 리스트
+    const getBoardList = (Object.keys(postData).length == 0)
+        ? boardSystem.getBoardList()
+        : boardSystem.getBoardList(postData);
+
+    getBoardList
+        .then((results) => {
+            res.status(200).json(results);
+        })
+        .catch((error) => {
+            res.status(400).json({success: false, message: 'Invalid Request'});
+        })
+});
 
 
 module.exports = router;
