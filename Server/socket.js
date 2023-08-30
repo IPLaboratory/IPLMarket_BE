d3ea4fbb0df858823c03da0b62a5d8277cb38730
ParkHyunSoo <parkhyuns00@naver.com>
2023-08-30 22:52:00 +0900

    TASK #41 : 안드로이드에서 전송한 동영상 파일 받아서 저장 - 안드로이드에서 Base64로 인코딩한 동영상 파일을 받아서 저장합니다. - 저장 완료되면 안드로이드에게 저장한 파일 이름을 전송합니다. - 저장 완료되면 ML에게 영상 파일 저장 경로, 파일 이름, 프롬프트 명령을 전송합니다.

--- Server/socket.js
+++ Server/socket.js
@@ -48,35 +48,38 @@ module.exports = (server, app) =>{
         //     })            
         // });
 
-        /* 클라이언트에서 Base64 인코딩된 파일 분할 전송
+        /* 안드로이드에서 Base64 인코딩된 비디오 파일 분할 전송
          * 수신하여 합친 후 디코딩해서 저장
         */
-        socket.on('sendFile', (encodedData) => {
-          const total = encodedData.total;
-          const count = encodedData.count;
+        socket.on('sendVideo', async (videoData) => {
+          const total = videoData.total;
+          const count = videoData.count;
           
           // 데이터가 모두 수신되었을 때
-          if (encodedData.count === encodedData.total){
-            fromClientData[socket.id] += encodedData.data; // 짜투리 데이터
+          if (videoData.count === videoData.total){
+            fromClientData[socket.id] += videoData.data; // 짜투리 데이터
             console.log(`[Done] Length : ${fromClientData[socket.id].length}`);
 
-            // Base64 디코딩
-            const decodedFile = Buffer.from(fromClientData[socket.id], 'base64');
-            const savePath = path.join(__dirname, 'public', 'receive.obj');
+            try {
+              // Base64 디코딩
+              const decodedFile = Buffer.from(fromClientData[socket.id], 'base64');
+              const fileName = uuid.v4() + '.mp4';
+              const filePath = path.join(__dirname, 'public', 'videos', fileName);
 
-            // 파일 저장
-            fs.writeFile(savePath, decodedFile, (err) => {
-              if (err){
-                console.log('Err : ' + err);
-              } else {
-                console.log('Save Complete : ' + savePath);
-              }
-            });
+              // 파일 저장
+              await fs.promises.writeFile(filePath, decodedFile);
+                
+              console.log('Save Complete : ',filePath);
 
-          } else{
+              socket.emit("video save complete", { success: true, fileName: fileName });
+              socket.emit("start modeling", { path: 'data/totoro.MOV' , name: fileName, prompt: videoData.prompt});
+            } catch (error) {
+              console.log('Error while decoding video ',error.message);
+            }
+          } else {
             // 데이터 수신중
             console.log(`Downloaded... ${(count / total * 100.0).toFixed(2)}%`);
-            fromClientData[socket.id] += encodedData.data;
+            fromClientData[socket.id] += videoData.data;
           }
         });
 
