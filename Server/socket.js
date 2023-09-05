a8f6cf1c253c47427896693c05397b2f1b2c32f8
ParkHyunSoo <parkhyuns00@naver.com>
2023-09-05 20:25:44 +0900

    TASK #43 : 안드로이드에서 비디오 전송 시 로컬에 저장되지 않는 버그를 수정했습니다. - 기존 코드에서 Base64 디코딩 시 버퍼에 담은 후 디코딩을 진행하는 방식이었습니다. - 부분적으로 전송하는 비디오 데이터 수신 시 수신과 동시에 디코딩 후 파일 이어쓰기로 파일을 바로 저장합니다.

--- Server/socket.js
+++ Server/socket.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const socketIO = require('socket.io');
+const uuid = require('uuid');
+
+// 딕셔너리 구조에서 value 값으로 key 값 가져오기
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
 
 
 module.exports = (server, app) =>{
     const http = require('http').createServer(app);
     const io = socketIO(server, {path:'/socket.io'}, {maxHttpBufferSizke:1e8});
-    const fromClientData = {};
+    const clientHasUUID = {};
     const who = {};
 
     // 소켓 연결
     io.on('connection', (socket) => {
-        console.log("User connect : " + socket.id);
-        fromClientData[socket.id] = '';
-
-        setTimeout(() => io.to(socket.id).emit('ml test', {'msg': path.join(__dirname, 'public', 'cake1.jpg'), 'job_id': 'totoro', 'prompt':'a totoro figure'}), 5000);
- 
-        // io.to(socket.id).emit('ml test', {'msg': path.join(__dirname, 'public', 'cake1.jpg'), 'job_id': 'totoro'})
+        console.log(`User connect [ID]: ${socket.id}`);
+        clientHasUUID[socket.id] = uuid.v4();
 
         socket.on('client registration', (data) => {
           who[socket.id] = data['device'];
@@ -25,68 +27,53 @@ module.exports = (server, app) =>{
 
         socket.on('ml message', (data) => {
           console.log(data);
-        })
+        });
 
-        // io.to(socket.id).emit('test', '...');
+        io.to(socket.id).emit('test', '...');
 
-        // 클라이언트에서 obj 파일 달라는 이벤트 수신시
-        // socket.on('connectReceive', (data) => {
-        //     console.log(data);
-
-        //     const filePath = path.join(__dirname, 'public', 'mesh.obj');
-        //     console.log(filePath);
-
-        //     fs.readFile(filePath, (err, fileContent) => {
-        //       if (err){
-        //         console.log(err);
-        //       } else {
-
-        //         // Base64 파일 인코딩
-        //         let base64File = fileContent.toString('base64');
-        //         socket.emit("testFile", {success: true, base64File});
-        //       }
-        //     })            
-        // });
-
-        /* 안드로이드에서 Base64 인코딩된 비디오 파일 분할 전송
+        /* 클라이언트에서 Base64 인코딩된 비디오 파일 분할 전송
          * 수신하여 합친 후 디코딩해서 저장
         */
         socket.on('sendVideo', async (videoData) => {
           const total = videoData.total;
           const count = videoData.count;
+          const fileName = clientHasUUID[socket.id] + '.mp4';
+          const filePath = path.join(__dirname, 'public', 'videos', fileName);
           
           // 데이터가 모두 수신되었을 때
           if (videoData.count === videoData.total){
-            fromClientData[socket.id] += videoData.data; // 짜투리 데이터
-            console.log(`[Done] Length : ${fromClientData[socket.id].length}`);
+            console.log('[Done] Download Finish!');
 
             try {
-              // Base64 디코딩
-              const decodedFile = Buffer.from(fromClientData[socket.id], 'base64');
-              const fileName = uuid.v4() + '.mp4';
-              const filePath = path.join(__dirname, 'public', 'videos', fileName);
-
-              // 파일 저장
-              await fs.promises.writeFile(filePath, decodedFile);
-                
+              // Base64 디코딩 후 짜투리 데이터 이어서 저장
+              const decodedFile = Buffer.from(videoData.data, 'base64');
+              await fs.promises.appendFile(filePath, decodedFile);
+              
               console.log('Save Complete : ',filePath);
-
+              
+              // 안드로이드에 저장된 비디오 파일 이름(=> 디렉토리 명)과 함께 저장 완료됐다는 메세지 전달
               socket.emit("video save complete", { success: true, fileName: fileName });
-              socket.emit("start modeling", { path: 'data/totoro.MOV' , name: fileName, prompt: videoData.prompt});
+
+              // ML에게 비디오 저장 완료됐으니 모델링 작업 시작해도 된다는 메세지 전달
+              io.to(getKeyByValue(who, 'ml')).emit("start modeling", { path: 'data/totoro.MOV' , name: fileName, prompt: videoData.prompt});
+
+              delete clientHasUUID[socket.id];
             } catch (error) {
               console.log('Error while decoding video ',error.message);
             }
           } else {
             // 데이터 수신중
             console.log(`Downloaded... ${(count / total * 100.0).toFixed(2)}%`);
-            fromClientData[socket.id] += videoData.data;
+
+            // 버퍼 용량 문제로 인해 즉시 인코딩 후 덮어쓰기 함
+            const decodedFile = Buffer.from(videoData.data, 'base64');
+            await fs.promises.appendFile(filePath, decodedFile);
           }
         });
 
         // 소켓 연결 해제시
         socket.on('disconnect', () => {
-            console.log("user disconnected");
-            delete fromClientData[socket.id];
+            console.log("user disconnected : ",socket.id);
         })
     });
 }
