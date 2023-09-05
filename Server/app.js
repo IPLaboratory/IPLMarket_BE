86ac83238103c2ea7b3af942d1355043a745cfe5
ParkHyunSoo <parkhyuns00@naver.com>
2023-09-05 23:09:23 +0900

    Style : 의미없는 주석 제거

--- Server/app.js
+++ Server/app.js
@@ -9,8 +9,8 @@ const likeRouter = require('./routes/like.js');
 const modelRouter = require('./routes/model.js');
 
 dotenv.config();
-app.use(express.urlencoded({extended : true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({limit: '100mb', extended : true}));
+app.use(bodyParser.json({limit: '100mb'}));
 app.use('/public', express.static('public'));
 
 /* 안드로이드와 HTTP 통신 */
@@ -30,61 +30,3 @@ const server = app.listen(process.env.PORT, () => {
 
 // 소켓 연결
 socketConnect(server, app);
-
-// const test = {
-//     id: 'phs1234',
-//     pwd: 'test'
-// }
-
-// loginSystem.registUser(test)
-//     .then((result) => {
-//         if (result) {
-//             console.log('Login Success');
-//         } else {
-//             console.log('Login Fail');
-//         }
-//     })
-//     .catch(error => {
-//         console.log('Login Fail ', error.message);
-//     });
-
-// const data = {
-//     title: 'test',
-//     content: 'test',
-//     image_name: 'cake1.jpg',
-//     user_id: 'test'
-// }
-
-
-// const test = {
-//     userId: 'test',
-//     postNum: '1'
-// }
-
-// like.checkLikeStatus(test)
-//     .then(hasLike => {
-//         if (hasLike) {
-//             return like.deleteLike(test);
-//         } else {
-//             return like.addLike(test);
-//         }
-//     })
-//     .then(result => {
-//         console.log('Like Success');
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
-
-// like.numberOfLikes(1)
-//     .then(counts => {
-//         console.log(counts[0].likeCount);
-//     })
-
-// like.myLikesList({id: 'test'})
-//     .then((results) => {
-//         console.log(results);
-//     })
-//     .catch(error => {
-//         console.log(error.message);
-//     })
